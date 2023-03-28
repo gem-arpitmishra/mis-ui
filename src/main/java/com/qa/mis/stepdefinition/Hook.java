bd8395bead0d00e6b81fb2847f532bc41d911835
Arpit Mishra <102647340+gem-arpitmishra@users.noreply.github.com>
2023-03-28 15:33:48 +0530

    Update Hook.java

--- src/main/java/com/qa/mis/stepdefinition/Hook.java
+++ src/main/java/com/qa/mis/stepdefinition/Hook.java
@@ -1,13 +1,36 @@
 package com.qa.mis.stepdefinition;
 
 import com.gemini.generic.exception.GemException;
+import com.gemini.generic.reporting.GemEcoUpload;
 import com.gemini.generic.ui.utils.DriverAction;
 import com.gemini.generic.ui.utils.DriverManager;
-import io.cucumber.java.Before;
+import com.gemini.generic.utils.GemJarGlobalVar;
+import com.gemini.generic.utils.GemJarUtils;
+import com.gemini.generic.reporting.GemTestReporter.*;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class Hook {
+import io.cucumber.java.Before;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class AthenaHook {
+
     @Before
     public void start() throws GemException {
-        DriverManager.setUpBrowser();
+//         DriverManager.setUpBrowser();
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.setHeadless(true);
+        options.addArguments("start-maximized"); // open Browser in maximized mode
+        options.addArguments("disable-infobars"); // disabling infobars
+        options.addArguments("--disable-extensions"); // disabling extensions
+        options.addArguments("--disable-gpu"); // applicable to windows os only
+        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        options.addArguments("--no-sandbox");
+        DriverManager.initializeChrome(options);
+        DriverAction.launchUrl(GemJarUtils.getGemJarConfigData("launchUrl"));
+        DriverAction.setImplicitTimeOut(Long.parseLong(GemJarGlobalVar.implicitTime));
+        DriverAction.setPageLoadTimeOut(Long.parseLong(GemJarGlobalVar.pageTimeout));
+        DriverAction.setScriptTimeOut(Long.parseLong(GemJarGlobalVar.scriptTimeout));
+
     }
 }
